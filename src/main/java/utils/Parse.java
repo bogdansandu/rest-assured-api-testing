5e484c9fed89da7d25a24638457f4bffcb951576
bogdansandu <bogdan.gabriel14@gmail.com>
2018-02-26 11:26:25 +0200

    Added reporting classes

---
+++ src/main/java/utils/Parse.java
@@ -0,0 +1,28 @@
+package utils;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+
+
+/**
+ * Created by bogdansandu on 2/26/18.
+ */
+public class Parse {
+    public static XmlPath rawToXML(Response r)
+    {
+
+
+        String respon=r.asString();
+        XmlPath x=new XmlPath(respon);
+        return x;
+
+    }
+
+    public static JsonPath rawToJson(Response r)
+    {
+        String respon=r.asString();
+        JsonPath x=new JsonPath(respon);
+        return x;
+    }
+}
